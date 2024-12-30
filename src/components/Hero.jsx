4e2812b7539fd96a60b53994281483e07b8f9049
Sushil G <guptask.7@gmail.com>
2024-12-30 14:11:49 -0500

    Updated hero section for header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -140,7 +140,7 @@ const Hero = () => {
 
                 <div className="absolute left-0 top-0 z-40 size-full">
                     <div className="mt-24 px-5 sm:px-10">
-                        <h1 className="special-font hero-heading text-blue-100">Redefi<b>n</b>e</h1>
+                        <h1 className="special-font hero-heading text-blue-100">Sus<b>h</b>il</h1>
                         <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
                             Enter the Cool Gaming Layer <br />
                             Unleash the Play Economy
